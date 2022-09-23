d7844eaa1762f83ca11fd4bc433df360236d0173
janethxuiz <janethruiz152@gmail.com>
2022-09-23 16:59:00 -0700

    schema db

--- db/schema.sql
+++ db/schema.sql
@@ -7,21 +7,24 @@ CREATE database employee_db;
 USE employee;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL() NOT NULL,
-    department_id INT(),
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT NULL
+    role_id INTEGER,
+    manager_id INTEGER NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employee(id) 
 );
